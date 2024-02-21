f9e69de19e4646acf675b927138fe7b47ee1abfa
craigwrong <95532896+craigwrong@users.noreply.github.com>
2024-02-21 15:41:34 +0000

    Ways of testing async peer-to-peer communication #170

--- test/bitcoin/playground/BitcoinPeerTests.swift
+++ test/bitcoin/playground/BitcoinPeerTests.swift
@@ -9,30 +9,35 @@ final class BitcoinPeerTests: XCTestCase {
         let service1 = BitcoinService()
         let service2 = BitcoinService()
 
-        let peer1 = BitcoinPeer(bitcoinService: service1, isClient: false)
-        let peer2 = BitcoinPeer(bitcoinService: service2, isClient: true)
+        let serverPeer = BitcoinPeer(bitcoinService: service1, isClient: false)
+        let clientPeer = BitcoinPeer(bitcoinService: service2, isClient: true)
 
         let serverTask = Task {
             var count = 0
-            for await message in await peer1.messagesOut {
-                await peer2.messagesIn.send(message)
+            for await message in await serverPeer.messagesOut {
+                await clientPeer.messagesIn.send(message)
                 if count == 1 { break }
                 count += 1
             }
         }
         let clientTask = Task {
             var count = 0
-            for await message in await peer2.messagesOut {
-                await peer1.messagesIn.send(message)
+            for await message in await clientPeer.messagesOut {
+                await serverPeer.messagesIn.send(message)
                 if count == 1 { break }
                 count += 1
             }
         }
+
         Task {
-            try await peer1.start()
-        }
-        Task {
-            try await peer2.start()
+            try await withThrowingDiscardingTaskGroup {
+                $0.addTask {
+                    try await serverPeer.start()
+                }
+                $0.addTask {
+                    try await clientPeer.start()
+                }
+            }
         }
 
         await withDiscardingTaskGroup {
@@ -44,11 +49,187 @@ final class BitcoinPeerTests: XCTestCase {
             }
         }
 
-        let peer1Handshake = await peer1.handshakeComplete
-        let peer2Handshake = await peer2.handshakeComplete
-        XCTAssert(peer1Handshake)
-        XCTAssert(peer2Handshake)
-        try await peer2.stop()
-        try await peer1.stop()
+        let serverHandshake = await serverPeer.handshakeComplete
+        await Task.yield()
+        let clientHandshake = await clientPeer.handshakeComplete
+        XCTAssert(serverHandshake)
+        XCTAssert(clientHandshake)
+        try await clientPeer.stop()
+        try await serverPeer.stop()
+    }
+
+    func testHandshakeSteps() async throws {
+        let service1 = BitcoinService()
+        let service2 = BitcoinService()
+
+        let serverPeer = BitcoinPeer(bitcoinService: service1, isClient: false)
+        let clientPeer = BitcoinPeer(bitcoinService: service2, isClient: true)
+
+        var serverMessages = await serverPeer.messagesOut.makeAsyncIterator()
+        var clientMessages = await clientPeer.messagesOut.makeAsyncIterator()
+
+        Task {
+            try await serverPeer.start()
+        }
+        Task {
+            try await clientPeer.start()
+        }
+
+        guard let message = await clientMessages.next() else {
+            XCTFail(); return
+        }
+        XCTAssertEqual(message.command, .version)
+        await serverPeer.messagesIn.send(message)
+
+        guard let message = await serverMessages.next() else {
+            XCTFail(); return
+        }
+        XCTAssertEqual(message.command, .version)
+        await clientPeer.messagesIn.send(message)
+
+        guard let message = await clientMessages.next() else {
+            XCTFail(); return
+        }
+        XCTAssertEqual(message.command, .verack)
+        await serverPeer.messagesIn.send(message)
+
+        var handshook = await serverPeer.handshakeComplete
+        XCTAssertFalse(handshook)
+
+        guard let message = await serverMessages.next() else {
+            XCTFail(); return
+        }
+        XCTAssertEqual(message.command, .verack)
+
+        handshook = await serverPeer.handshakeComplete
+        XCTAssert(handshook)
+
+        handshook = await clientPeer.handshakeComplete
+        XCTAssertFalse(handshook)
+
+        await clientPeer.messagesIn.send(message)
+        await Task.yield()
+        handshook = await clientPeer.handshakeComplete
+        XCTAssert(handshook)
+
+        try await clientPeer.stop()
+        try await serverPeer.stop()
+    }
+
+    func testPingPong() async throws {
+        let nodeA = BitcoinService()
+        let nodeB = BitcoinService()
+
+        let serverPeer = BitcoinPeer(bitcoinService: nodeA, isClient: false)
+        let clientPeer = BitcoinPeer(bitcoinService: nodeB, isClient: true)
+
+        let serverTask = Task {
+            for await message in await serverPeer.messagesOut {
+                if message.command == .pong {
+                    return Message?.some(message)
+                }
+                await clientPeer.messagesIn.send(message)
+            }
+            return Message?.none
+        }
+
+        let clientTask = Task {
+            for await message in await clientPeer.messagesOut {
+                await serverPeer.messagesIn.send(message)
+                if message.command == .ping {
+                    return Message?.some(message)
+                }
+            }
+            return Message?.none
+        }
+
+        Task {
+            try await withThrowingDiscardingTaskGroup {
+                $0.addTask {
+                    try await serverPeer.start()
+                }
+                $0.addTask {
+                    try await clientPeer.start()
+                }
+            }
+        }
+
+        // Initiate the ping
+        try await clientPeer.sendPing()
+
+        let (pingMessage, pongMessage) = try await withThrowingTaskGroup(of: Message?.self, returning: (Message, Message).self) { group in
+            group.addTask {
+                await serverTask.value
+            }
+            group.addTask {
+                await clientTask.value
+            }
+            guard let firstResult = try await group.next(),
+                  let secondResult = try await group.next(),
+                  let firstMessage = firstResult,
+                  let secondMessage = secondResult else {
+                XCTFail()
+                throw XCTSkip()
+            }
+            return if firstMessage.command == .ping { (firstMessage, secondMessage) } else { (secondMessage, firstMessage) }
+        }
+
+        try await clientPeer.stop()
+        try await serverPeer.stop()
+
+        let ping = try XCTUnwrap(Ping(pingMessage.payload))
+        let pong = try XCTUnwrap(Pong(pongMessage.payload))
+        XCTAssertEqual(ping.nonce, pong.nonce)
+    }
+
+    func testPingPongSteps() async throws {
+        let service1 = BitcoinService()
+        let service2 = BitcoinService()
+
+        let serverPeer = BitcoinPeer(bitcoinService: service1, isClient: false)
+        let clientPeer = BitcoinPeer(bitcoinService: service2, isClient: true)
+
+        var serverMessages = await serverPeer.messagesOut.makeAsyncIterator()
+        var clientMessages = await clientPeer.messagesOut.makeAsyncIterator()
+
+        Task {
+            try await serverPeer.start()
+        }
+        Task {
+            try await clientPeer.start()
+        }
+
+        guard let message = await clientMessages.next() else { XCTFail(); return }
+        await serverPeer.messagesIn.send(message)
+        guard let message = await serverMessages.next() else { XCTFail(); return }
+        await clientPeer.messagesIn.send(message)
+
+        guard let message = await clientMessages.next() else { XCTFail(); return }
+        await serverPeer.messagesIn.send(message)
+        guard let message = await serverMessages.next() else { XCTFail(); return }
+        await clientPeer.messagesIn.send(message)
+
+        Task {
+            try await clientPeer.sendPing()
+        }
+        guard let message = await clientMessages.next() else { XCTFail(); return }
+        XCTAssertEqual(message.command, .ping)
+        let ping = try XCTUnwrap(Ping(message.payload))
+        await serverPeer.messagesIn.send(message)
+
+        guard let message = await serverMessages.next() else { XCTFail(); return }
+        XCTAssertEqual(message.command, .pong)
+        let pong = try XCTUnwrap(Pong(message.payload))
+        XCTAssertEqual(ping.nonce, pong.nonce)
+        await clientPeer.messagesIn.send(message)
+
+        var lastPingNonce = await clientPeer.lastPingNonce
+        XCTAssertEqual(pong.nonce, lastPingNonce)
+        await Task.yield()
+        lastPingNonce = await clientPeer.lastPingNonce
+        XCTAssertNil(lastPingNonce)
+
+        try await clientPeer.stop()
+        try await serverPeer.stop()
     }
 }
