aa7043249ff7c7e1cdfe88ff7a4af340b89abcdd
Workchain Manipulator ⚡️ <95532896+craigwrong@users.noreply.github.com>
2024-03-07 15:41:43 +0000

    BIP133: feefilter message implemented both directions #168 (#189)
    Handshake and ping-pong tests updated.

--- test/bitcoin/transport/NodeServiceTests.swift
+++ test/bitcoin/transport/NodeServiceTests.swift
@@ -3,11 +3,28 @@ import XCTest
 
 final class NodeServiceTests: XCTestCase {
 
-     func testHandshake() async throws {
+    /// Tests handshake and extended post-handshake exchange.
+    ///
+    /// Outbound connection sequence:
+    ///
+    ///     -> version (we send the first message)
+    ///     -> wtxidrelay
+    ///     -> sendaddrv2
+    ///     <- version
+    ///     -> verack
+    ///     -> getaddr
+    ///     <- verack
+    ///     -> sendcmpct
+    ///     -> ping
+    ///     -> getheaders
+    ///     -> feefilter
+    ///     <- pong
+    ///
+    func testHandshake() async throws {
         let serviceA = BitcoinService()
         let serviceB = BitcoinService()
-        let serverNode = NodeService(bitcoinService: serviceA)
-        let clientNode = NodeService(bitcoinService: serviceB)
+        let serverNode = NodeService(bitcoinService: serviceA, feeFilterRate: 2)
+        let clientNode = NodeService(bitcoinService: serviceB, feeFilterRate: 3)
         let peerInServer = await serverNode.addPeer(host: "", port: 0)
         let peerInClient = await clientNode.addPeer(host: "", port: 0, incoming: false)
         var serverMessages = await serverNode.getChannel(for: peerInServer).makeAsyncIterator()
@@ -16,56 +33,82 @@ final class NodeServiceTests: XCTestCase {
         Task {
             await clientNode.connect(peerInClient)
         }
-        guard let message0 = await clientMessages.next() else { XCTFail(); return }
+        guard let clientVersion = await clientMessages.next() else { XCTFail(); return }
         await Task.yield()
-        XCTAssertEqual(message0.command, .version)
+        XCTAssertEqual(clientVersion.command, .version)
 
         Task {
-            try await serverNode.processMessage(message0, from: peerInServer)
+            try await serverNode.processMessage(clientVersion, from: peerInServer)
         }
-        guard let message1 = await serverMessages.next() else { XCTFail(); return }
+        guard let serverVersion = await serverMessages.next() else { XCTFail(); return }
         await Task.yield()
-        XCTAssertEqual(message1.command, .version)
-        guard let message2 = await serverMessages.next() else { XCTFail(); return }
+        XCTAssertEqual(serverVersion.command, .version)
+        guard let serverSendAddrV2 = await serverMessages.next() else { XCTFail(); return }
         await Task.yield()
-        XCTAssertEqual(message2.command, .sendaddrv2)
-        guard let message3 = await serverMessages.next() else { XCTFail(); return }
+        XCTAssertEqual(serverSendAddrV2.command, .sendaddrv2)
+        guard let serverVerack = await serverMessages.next() else { XCTFail(); return }
         await Task.yield()
-        XCTAssertEqual(message3.command, .verack)
+        XCTAssertEqual(serverVerack.command, .verack)
 
         Task {
-            try await clientNode.processMessage(message1, from: peerInClient)
+            try await clientNode.processMessage(serverVersion, from: peerInClient)
         }
-        guard let message4 = await clientMessages.next() else { XCTFail(); return }
+        guard let clientSendAddrV2 = await clientMessages.next() else { XCTFail(); return }
         await Task.yield()
-        XCTAssertEqual(message4.command, .sendaddrv2)
-        guard let message5 = await clientMessages.next() else { XCTFail(); return }
+        XCTAssertEqual(clientSendAddrV2.command, .sendaddrv2)
+        guard let clientVerack = await clientMessages.next() else { XCTFail(); return }
         await Task.yield()
-        XCTAssertEqual(message5.command, .verack)
+        XCTAssertEqual(clientVerack.command, .verack)
 
-        var versionReceived = await clientNode.peers[peerInClient]!.receivedVersion
+        let versionReceived = await clientNode.peers[peerInClient]!.receivedVersion
         XCTAssert(versionReceived)
 
-        try await clientNode.processMessage(message2, from: peerInClient)
+        try await clientNode.processMessage(serverSendAddrV2, from: peerInClient)
         var v2Addr = await clientNode.peers[peerInClient]!.receivedV2AddressPreference
         XCTAssert(v2Addr)
 
-        try await clientNode.processMessage(message3, from: peerInClient)
+        Task {
+            try await clientNode.processMessage(serverVerack, from: peerInClient)
+        }
+        guard let clientFeeFilterMessage = await clientMessages.next() else { XCTFail(); return }
+        await Task.yield()
+        XCTAssertEqual(clientFeeFilterMessage.command, .feefilter)
+        let clientFeeFilter = try XCTUnwrap(FeeFilterMessage(clientFeeFilterMessage.payload))
+        let clientFeeRate = await clientNode.feeFilterRate
+        XCTAssertEqual(clientFeeFilter.feeRate, clientFeeRate)
+
         var veracked = await clientNode.peers[peerInClient]!.receivedVersionAck
         XCTAssert(veracked)
         var handshook = await clientNode.peers[peerInClient]!.handshakeComplete
         XCTAssert(handshook)
 
-        try await serverNode.processMessage(message4, from: peerInServer)
+        try await serverNode.processMessage(clientSendAddrV2, from: peerInServer)
         v2Addr = await serverNode.peers[peerInServer]!.receivedV2AddressPreference
         XCTAssert(v2Addr)
 
-        try await serverNode.processMessage(message5, from: peerInServer)
+        Task {
+            try await serverNode.processMessage(clientVerack, from: peerInServer)
+        }
+        guard let serverFeeFilterMessage = await serverMessages.next() else { XCTFail(); return }
+        await Task.yield()
+        XCTAssertEqual(serverFeeFilterMessage.command, .feefilter)
+        let serverFeeFilter = try XCTUnwrap(FeeFilterMessage(serverFeeFilterMessage.payload))
+        let serverFeeRate = await serverNode.feeFilterRate
+        XCTAssertEqual(serverFeeFilter.feeRate, serverFeeRate)
+
         veracked = await serverNode.peers[peerInServer]!.receivedVersionAck
         XCTAssert(veracked)
 
         handshook = await serverNode.peers[peerInServer]!.handshakeComplete
         XCTAssert(handshook)
+
+        try await serverNode.processMessage(clientFeeFilterMessage, from: peerInServer) // No response expected
+        let serverFeeRateForClient = await serverNode.peers[peerInServer]!.feeFilterRate
+        XCTAssertEqual(clientFeeRate, serverFeeRateForClient)
+
+        try await clientNode.processMessage(serverFeeFilterMessage, from: peerInClient) // No response expected
+        let clientFeeRateForServer = await clientNode.peers[peerInClient]!.feeFilterRate
+        XCTAssertEqual(serverFeeRate, clientFeeRateForServer)
     }
 
     func testPingPong() async throws {
@@ -79,31 +122,43 @@ final class NodeServiceTests: XCTestCase {
         var clientMessages = await clientNode.getChannel(for: peerInClient).makeAsyncIterator()
 
         Task { await clientNode.connect(peerInClient) }
-        guard let message0 = await clientMessages.next() else { XCTFail(); return }
+        guard let clientVersion = await clientMessages.next() else { XCTFail(); return }
         await Task.yield()
 
         Task {
-            try await serverNode.processMessage(message0, from: peerInServer)
+            try await serverNode.processMessage(clientVersion, from: peerInServer)
         }
-        guard let message1 = await serverMessages.next() else { XCTFail(); return }
+        guard let serverVersion = await serverMessages.next() else { XCTFail(); return }
         await Task.yield()
-        guard let message2 = await serverMessages.next() else { XCTFail(); return }
+        guard let serverSendAddrV2 = await serverMessages.next() else { XCTFail(); return }
         await Task.yield()
-        guard let message3 = await serverMessages.next() else { XCTFail(); return }
+        guard let serverVerack = await serverMessages.next() else { XCTFail(); return }
         await Task.yield()
 
         Task {
-            try await clientNode.processMessage(message1, from: peerInClient)
+            try await clientNode.processMessage(serverVersion, from: peerInClient)
         }
-        guard let message4 = await clientMessages.next() else { XCTFail(); return }
+        guard let clientSendAddrV2 = await clientMessages.next() else { XCTFail(); return }
         await Task.yield()
-        guard let message5 = await clientMessages.next() else { XCTFail(); return }
+        guard let clientVerack = await clientMessages.next() else { XCTFail(); return }
         await Task.yield()
 
-        try await clientNode.processMessage(message2, from: peerInClient)
-        try await clientNode.processMessage(message3, from: peerInClient)
-        try await serverNode.processMessage(message4, from: peerInServer)
-        try await serverNode.processMessage(message5, from: peerInServer)
+        try await clientNode.processMessage(serverSendAddrV2, from: peerInClient)
+        Task {
+            try await clientNode.processMessage(serverVerack, from: peerInClient)
+        }
+        guard let clientFeeFilter = await clientMessages.next() else { XCTFail(); return }
+        await Task.yield()
+
+        try await serverNode.processMessage(clientSendAddrV2, from: peerInServer)
+        Task {
+            try await serverNode.processMessage(clientVerack, from: peerInServer)
+        }
+        guard let serverFeeFilter = await serverMessages.next() else { XCTFail(); return }
+        await Task.yield()
+
+        try await serverNode.processMessage(clientFeeFilter, from: peerInServer)
+        try await clientNode.processMessage(serverFeeFilter, from: peerInClient)
 
         Task {
             await clientNode.pingAll()
