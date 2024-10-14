e108235649e582e533c77c8d494c72f5c47ee4b2
craigwrong <95532896+craigwrong@users.noreply.github.com>
2024-10-14 16:10:52 +0100

    BIP152 WIP - Relay empty blocks
    Partial implementation of #252

--- src/bitcoin-node/services/NodeService+ServiceLifecycle.swift
+++ src/bitcoin-node/services/NodeService+ServiceLifecycle.swift
@@ -6,14 +6,14 @@ private let logger = Logger(label: "swift-bitcoin.node")
 
 extension NodeService: Service {
     public func run() async throws {
-        let blocks = await bitcoinService.subscribeToBlocks()
-        self.blocks = blocks
+        await start()
+        guard let blocks else { await stop(); return }
+
         await withGracefulShutdownHandler {
             for await block in blocks.cancelOnGracefulShutdown() {
-                // If we have peers, send them the block
-                logger.info("New block (might send to peer):\n\t\(block.hash.hex)]")
+                await handleBlock(block)
             }
-            await self.bitcoinService.unsubscribe(blocks)
+            await stop()
         } onGracefulShutdown: {
             logger.info("BitcoinNode Service shutting down gracefully")
         }
